57e32550d5f7957bc208f19a3f3b33fce088c9ca
AnshikaSharma123-tech <anshikamudgal645@gmail.com>
2025-09-11 15:48:50 +0530

    Add files via upload

---
+++ SquareFill.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class SquareFill {
+  public static void main(String ar[]) {
+    int n=6 , i , j ;
+    for (i=0; i<n; i++) {
+        for (j=0;j<n;j++) {
+            System.out.print("*");
+
+        }
+        System.out.println();
+    } 
+  }  
+}
