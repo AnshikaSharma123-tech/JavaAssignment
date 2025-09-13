cbd0036ed0a99ea9142c5237ea452fac5ecf8de0
AnshikaSharma123-tech <anshikamudgal645@gmail.com>
2025-09-13 22:32:49 +0530

    Add files via upload

---
+++ RemoveSpace.java
@@ -0,0 +1,16 @@
+public class RemoveSpace {
+    public static void main(String[] args) {   
+        String s = "This is rdec sec f";
+       for (int i=0 ; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                
+            }
+            else {
+                System.out.print(s.charAt(i));
+            }
+        }
+        
+    }
+}
+
+
